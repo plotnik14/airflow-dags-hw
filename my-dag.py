6b555a5da0b2e9e4ffb260fd0ab22b9104efa898
Alexander <plotnikov93av@gmail.com>
2021-07-01 16:08:25 +0300

    Update my-dag.py

--- my-dag.py
+++ my-dag.py
@@ -2,60 +2,22 @@
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+from airflow.models import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.utils.dates import days_ago
 
-"""
-Example DAG demonstrating setting up inter-DAG dependencies using ExternalTaskSensor and
-ExternalTaskMarker
-In this example, child_task1 in example_external_task_marker_child depends on parent_task in
-example_external_task_marker_parent. When parent_task is cleared with "Recursive" selected,
-the presence of ExternalTaskMarker tells Airflow to clear child_task1 and its
-downstream tasks.
-"""
-
-import datetime
-
-from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.sensors.external_task_sensor import ExternalTaskMarker, ExternalTaskSensor
-
-start_date = datetime.datetime(2015, 1, 1)
+args = {
+    'owner': 'Alex P',
+}
 
 with DAG(
-    dag_id="example_external_task_marker_parent",
-    start_date=start_date,
+    dag_id='airflow_hw',
+    default_args=args,
     schedule_interval=None,
-    tags=['example'],
-) as parent_dag:
-    # [START howto_operator_external_task_marker]
-    parent_task = ExternalTaskMarker(task_id="parent_task",
-                                     external_dag_id="example_external_task_marker_child",
-                                     external_task_id="child_task1")
-    # [END howto_operator_external_task_marker]
-
-with DAG(
-    dag_id="example_external_task_marker_child",
-    start_date=start_date,
-    schedule_interval=None,
-    tags=['example'],
-) as child_dag:
-    # [START howto_operator_external_task_sensor]
-    child_task1 = ExternalTaskSensor(task_id="child_task1",
-                                     external_dag_id=parent_dag.dag_id,
-                                     external_task_id=parent_task.task_id,
-                                     mode="reschedule")
-    # [END howto_operator_external_task_sensor]
-    child_task2 = DummyOperator(task_id="child_task2")
-    child_task1 >> child_task2
+    start_date=days_ago(2),
+    tags=['hw'],
+) as dag:
+   
+    submit_job = SparkSubmitOperator(
+        application="${SPARK_HOME}/examples/src/main/python/pi.py", task_id="submit_job"
+    )
